bbf0bdc3d1be66fe86cd3ca6cf39fa9f0bad17fe
DaffaHirza <3103120061@student.smktelkom-pwt.sch.id>
2022-10-26 13:16:25 +0700

    Book

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeloController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 
 
 /*
@@ -28,3 +29,4 @@ Route::get('Hallo', function(){
 
 Route::resource('halcontroller', HeloController::Class);
 Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class);
